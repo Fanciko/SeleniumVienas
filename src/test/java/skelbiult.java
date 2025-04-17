c41f88c87bd99ea1462d79dd49e647803ce39986
Fanciko <jonis161@gmail.com>
2025-04-17 08:46:25 +0300

    Duomenu surinkimas is svetainiu.

--- src/test/java/skelbiult.java
+++ src/test/java/skelbiult.java
@@ -1,129 +1,89 @@
-import com.sun.jdi.InternalException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class skelbiult {
-    private WebDriver driver;
-
-    @Test
-    public void getAvgPrices() throws InterruptedException {
-        driver();
-        goToWebsite();
-        acceptCookies();
-        searchAd();
-        countAd();
-    }
-
-    private void driver() {
-        driver = new ChromeDriver();
-    }
-
-    private void goToWebsite() {
+    public void acceptCookies(WebDriver driver){
         driver.get("https://skelbiu.lt");
+        WebElement acceptBtn = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        acceptBtn.click();
     }
+    @Test
+    public void dataGatherer(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        acceptCookies(driver);
 
-    private void acceptCookies() {
-        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
-    }
-
-    private void searchAd() {
-        driver.findElement(By.id("searchKeyword")).sendKeys("verpimo ratelis");
-        driver.findElement(By.id("searchButton")).click();
-    }
-
-    private void countAd() throws InterruptedException {
-            Thread.sleep(500);
-                                                                         //*[@id="body-container"]/div[2]/div[1]/ul/li/span
-        String adsQuatity = driver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
-        double sum = 0;
-        int count = 0;
-        boolean nextPage = true;
-        while (nextPage) {
-            WebElement container = driver.findElement(By.className("standard-list-container"));
-            List<WebElement> contentBlocks = container.findElements(By.className("content-block"));
-            Thread.sleep(1000);
-
-            for (int i = 0; i < contentBlocks.size(); i++) {
-                double price = getPrice(contentBlocks.get(i));
-                if (price > 0) {
-                    sum += price;
-                    count++;
-                }
+        String searchKeyword = "verpimo ratelis";
+        String baseUrl = "https://www.skelbiu.lt/skelbimai/";
+        List<String> urls = new ArrayList<>();
+        for (int i = 1; i <= 200; i++) {
+            String url = baseUrl + i + "?keywords=" + searchKeyword.replace(" ","%20");
+            driver.get(url);
+            String currentUrl = driver.getCurrentUrl();
+            if(!url.equals(currentUrl)){
+                break;
             }
-            nextPage = nextPage();
+            urls.addAll(getUrls(driver));
         }
-
-        System.out.println("skelbimu pavyko rasti: " + adsQuatity + ", daikto vidurkio kaina: " + sum / count + ".");
+        System.out.println(urls.size());
+        getUserData(driver, urls);
     }
 
-    public double getPrice(WebElement block) {
-        try {
-            String price = block.findElement(By.className("price")).getText();
-            price = price.replaceAll("\\D+", "");
-            return Double.parseDouble(price);
-        } catch (Exception e) {
-            System.out.println("nebuvo kainos");
+    private void getUserData(WebDriver driver, List<String> urls) {
+        for (String url : urls) {
+            driver.get(url);
+            try {
+                System.out.println(driver.findElement(By.cssSelector(".profile-data > .name")).getText());
+            }catch (Exception e){}
         }
-        return 0;
     }
 
-    private boolean nextPage() {
-        try { // spausti kita puslapi
-            driver.findElement(By.linkText("»")).click();
-            return true;
-        } catch (Exception e) {
-            return false;
+    private List<String> getUrls(WebDriver driver) {
+        List<WebElement> urlsElements = driver.findElements(By.cssSelector(".standard-list-container > div > a"));
+        List<String> urls = new ArrayList<>();
+        for (WebElement urlElem:urlsElements   ) {
+            String url = "https://www.skelbiu.lt" + urlElem.getDomAttribute("href");
+            urls.add(url);
         }
+        return urls;
     }
+
+
+    //    @Test
+    public void openWebsite() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://skelbiu.lt");
+        Thread.sleep(500);
+        WebElement acceptBtn = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        acceptBtn.click();
+        driver.findElement(By.id("searchKeyword")).sendKeys("kengura");
+        driver.findElement(By.id("searchButton")).click();
+        Thread.sleep(500);
+        String adsQuatity = driver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
+        System.out.println("radome " + adsQuatity + " skelbimu.");
+        WebElement container = driver.findElement(By.className("standard-list-container"));
+        List<WebElement> contentBlocks = container.findElements(By.className("content-block"));
+        Thread.sleep(1000);
+        for (int i = 0; i < contentBlocks.size(); i++) {
+            String price = "0";
+            try {
+                price = contentBlocks.get(i).findElement(By.className("price")).getText();
+            }catch (Exception e){
+                System.out.println("nebuvo kainos");
+            }
+            System.out.println(price);
+        }
+//        driver.quit();
+    }
+
+
 
 }
-
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://skelbiu.lt");
-//        Thread.sleep(500);
-//        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
-//
-//        driver.findElement(By.id("searchKeyword")).sendKeys("telefonas");
-//        driver.findElement(By.id("searchButton")).click();
-//        Thread.sleep(500);
-//        String adsQuatity = driver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
-//        System.out.println("Radome pirmame puslapyje " + adsQuatity + " skelbimus");
-//
-//        double sum = 0;
-//        int count = 0;
-//        while (true) {
-//            WebElement container = driver.findElement(By.className("standard-list-container"));
-//            List<WebElement> contentBlocks = container.findElements(By.className("content-block"));
-//            Thread.sleep(1000);
-//
-//            for (int i = 0; i < contentBlocks.size(); i++) {
-//                String price = "0";
-//                try {
-//                    price = contentBlocks.get(i).findElement(By.className("price")).getText();
-//                } catch (Exception e) {
-//                    System.out.println("nebuvo kainos");
-//                }
-//                price = price.replaceAll("\\D+", "");
-//                try {
-//                    sum += Double.parseDouble(price);
-//                    count++;
-//                } catch (Exception e) {
-//                }
-//            }
-//            try { // spausti kita puslapi
-//                driver.findElement(By.linkText("»")).click();
-//            } catch (Exception e) {
-//                break;
-//            }
-//        }
-
-
-//        System.out.println("Kainu vidurkis: " + sum / count);
-//        Thread.sleep(500);
-// }
-
